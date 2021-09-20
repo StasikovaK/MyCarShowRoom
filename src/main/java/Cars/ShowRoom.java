9347fbc1f3be1a48d93217f1c5705060c3433dbf
Jekaterina Stasikova <stasikova.ekaterina@gmail.com>
2021-09-20 19:08:27 +0300

    20 september question find by manufacturer errors.

--- src/main/java/Cars/ShowRoom.java
+++ src/main/java/Cars/ShowRoom.java
@@ -1,13 +1,15 @@
 package Cars;
 
+
 import java.util.ArrayList;
 
 public class ShowRoom {
     private ArrayList<Car> cars = new ArrayList<Car>();
 
     //add cars
-    public String addCar(Car car){
+    public String addCar(Car car) {
         this.cars.add(car);
+        System.out.println("\n");
         return car.name + " added successfully";
     }
     //getAllCars (view cars)
@@ -19,19 +21,61 @@ public class ShowRoom {
 
     //getSingleCar (view specific car)
 
-    public Car getSingleCar (int carID) {
+    public Car getSingleCar(int carID) {
         return this.cars.get(carID);
     }
     //removeCar
 
 
-
-    public String removeCar(int carID){
-        try{
+    public String removeCar(int carID) {
+        try {
             cars.remove(carID);
         } catch (Exception ex) {
             return "Unable to remove specified car";
         }
         return "Car Removed Successfully";
+    }
+
+    public Car findByName(String name) {
+        //Loop over the cars in the showroom and check each name equals to the specified name
+        //Return the car if  true.
+        for (Car car : cars) {
+            if (car.name.equals(name)) {
+                return car;
+            }
+        }
+        return null; //
+    }
+    //create array list of cars of the same manufacturer name array list .
+
+    public ArrayList<Car> findByManufacturer(String manufacturer) {
+        ArrayList <Car> list = new ArrayList<Car>();
+        for (Car car : cars) {
+            if (car.manufacturer.equals(manufacturer)) {
+               list.add(car);
+            }
+        }
+        return list; // in case there is nothing to return it returns nothing
+    }
+
+    public Car findByType(String type) {
+        for (Car car : cars) {
+            if (car.type.equals(type)) {
+                return car;
+            }
+        }
+        return null; //
+    }
+
+    public Car updateCar(int carIndex, Car car) {
+        return cars.set(carIndex, car);
+    }
+
+    public Car updateCarByManufacturer(int carIndex, Car car) {
+        return cars.set(carIndex, car);
+    }
+
+    public Car updateCarByType(int carIndex, Car car) {
+        return cars.set(carIndex, car);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,16 +19,22 @@ public class Main {
         String userInput = "";
 
         do {
-            System.out.println("\nWelcome to the showroom, please choose an activity: ");
+            System.out.println("\n \n \nWelcome to the showroom, please choose an activity: ");
             System.out.println("1. Add Car");
             System.out.println("2. View All Cars");
             System.out.println("3. View Specific Car");
             System.out.println("4. Remove Car");
+            System.out.println("5. Remove Car By Name");
+            System.out.println("6. Find Car By Name");
+            System.out.println("7. Find Car By Manufacturer");
+            System.out.println("8. Find Car By Type");
+            System.out.println("9. Update Car By Name");
+            System.out.println("10. Update Car By Manufacturer");
+            System.out.println("11. Update Car By Type");
             System.out.println("\nType 'Quit' to end program");
 
             System.out.println("Choose A Number:");
-            userInput = scanner.nextLine();
-            //next line method works for Strings
+            userInput = scanner.nextLine(); //nextLine method works for Strings
 
             switch(userInput) {
                 case "Quit":
@@ -49,6 +55,27 @@ public class Main {
                 case "4":
                     //remove car
                     removeCar();
+                    break;
+                case "5":
+                    removeCarByName();
+                    break;
+                case "6":
+                    findCarByName();
+                    break;
+                case "7":
+                    findCarByManufacturer();
+                    break;
+                case "8":
+                    findCarByType();
+                    break;
+                case "9":
+                    updateCar();
+                    break;
+                case "10":
+                    updateCarByManufacturer();
+                    break;
+                case "11":
+                    updateCarByType();
                     break;
                 default:
                     break;
@@ -101,7 +128,6 @@ public class Main {
         System.out.println("Car Manufacturer: " + car.manufacturer);
         System.out.println("Car Type: " + car.type);
 
-
     }
 
     void removeCar() {
@@ -116,4 +142,170 @@ public class Main {
     }
     
     //this is a comment made on the remote server and ain't yet on my machine
+
+
+    //This is the second comment
+    void findCarByName() {
+        System.out.println("Find car by name..");
+        System.out.println("Enter car name: ");
+
+        String carName = scanner.nextLine();
+
+        Car car = showRoom.findByName(carName);
+
+        System.out.println("car ID: " + car.ID);
+        System.out.println("car name: " + car.name);
+        System.out.println("car manufacturer: " + car.manufacturer);
+        System.out.println("car type: " + car.type);
+
+    }
+    void findCarByManufacturer() {
+        System.out.println("Find car by manufacturer..");
+        System.out.println("Enter car manufacturer: ");
+
+        String carManufacturer = scanner.nextLine();
+
+       ArrayList<Car> list = showRoom.findByManufacturer(carManufacturer);//change this to recieve an array list of cars
+
+        for (Car car: list) {
+                    System.out.println(car.name + " \t \t \t" + car.manufacturer + " \t \t \t" + car.type);
+
+        //
+//another loop to print each car in the array list (check list all cars method
+//        System.out.println("car ID: " + car.ID);
+//        System.out.println("car name: " + car.name);
+//        System.out.println("car manufacturer: " + car.manufacturer);
+//        System.out.println("car type: " + car.type);
+
+    }
+    void findCarByType() {
+        System.out.println("Find car by type..");
+        System.out.println("Enter car type: ");
+
+        String carType = scanner.nextLine();
+
+        Car car = showRoom.findByType(carType);
+
+        System.out.println("car ID: " + car.ID);
+        System.out.println("car name: " + car.name);
+        System.out.println("car manufacturer: " + car.manufacturer);
+        System.out.println("car type: " + car.type);
+
+    }
+
+    void removeCarByName() {
+        System.out.println("Remove Car By Name..");
+        System.out.println("Enter  Car Name:");
+
+        String carName = scanner.nextLine();
+        Car car = showRoom.findByName(carName);
+
+        int carIndex = showRoom.getAllCars().indexOf(car);
+
+        System.out.println(showRoom.removeCar(carIndex));
+    }
+
+
+        void updateCar(){
+            System.out.println("Update car");
+            System.out.println("Enter car name: ");
+            String carName = scanner.nextLine();
+
+            Car car = showRoom.findByName(carName);
+
+            int carIndex = showRoom.getAllCars().indexOf(car);
+            System.out.println("Enter the property you would like to change: ");
+            String property = scanner.nextLine();
+
+            System.out.println("Enter the new value of: " + property);
+
+            String value = scanner.nextLine();
+
+            switch (property) {
+                case "name":
+                    car.name = value;
+                    break;
+                case "manufacturer":
+                    car.manufacturer = value;
+                    break;
+                case "type":
+                    car.type = value;
+                    break;
+                default:
+                    System.out.println("Please provide a valid property");
+                    break;
+            }
+
+            showRoom.updateCar(carIndex, car);
+            System.out.println(car.name + " updated successfully");
+
+        }
+
+//    void updateCarByManufacturer(){
+//        System.out.println("Update car");
+//        System.out.println("Enter car manufacturer: ");
+//        String carManufacturer = scanner.nextLine();
+//
+//        Car car = showRoom.findByManufacturer(carManufacturer);
+//
+//        int carIndex = showRoom.getAllCars().indexOf(car);
+//        System.out.println("Enter the property you would like to change: ");
+//        String property = scanner.nextLine();
+//
+//        System.out.println("Enter the new value of: " + property);
+//
+//        String value = scanner.nextLine();
+//
+//        switch (property) {
+//            case "name":
+//                car.name = value;
+//                break;
+//            case "manufacturer":
+//                car.manufacturer = value;
+//                break;
+//            case "type":
+//                car.type = value;
+//                break;
+//            default:
+//                System.out.println("Please provide a valid property");
+//                break;
+//        }
+//
+//        showRoom.updateCarByManufacturer(carIndex, car);
+//        System.out.println(car.name + " updated successfully");
+//    }
+
+    void updateCarByType(){
+        System.out.println("Update car");
+        System.out.println("Enter car manufacturer: ");
+        String carType = scanner.nextLine();
+
+        Car car = showRoom.findByType(carType);
+
+        int carIndex = showRoom.getAllCars().indexOf(car);
+        System.out.println("Enter the property you would like to change: ");
+        String property = scanner.nextLine();
+
+        System.out.println("Enter the new value of: " + property);
+
+        String value = scanner.nextLine();
+
+        switch (property) {
+            case "name":
+                car.name = value;
+                break;
+            case "manufacturer":
+                car.manufacturer = value;
+                break;
+            case "type":
+                car.type = value;
+                break;
+            default:
+                System.out.println("Please provide a valid property");
+                break;
+        }
+
+        showRoom.updateCarByType(carIndex, car);
+        System.out.println(car.name + " updated successfully");
+    }
 }
